084928afd650981d8eaa5f92dab5c1c133773faa
Benbadis SLIMANI <slimani.b@gmail.com>
2024-12-10 22:19:57 +0100

    Fix formatting in ApplicationConfig.java by adding a new line for improved readability

--- src/main/java/com/gamerecs/gamerecs_backend/config/ApplicationConfig.java
+++ src/main/java/com/gamerecs/gamerecs_backend/config/ApplicationConfig.java
@@ -2,6 +2,7 @@ package com.gamerecs.gamerecs_backend.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+
 import lombok.Getter;
 import lombok.Setter;
 
